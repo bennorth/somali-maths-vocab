535e2997b066f75313b66f020d2f31d22b060c58
Ben North <ben@redfrontdoor.org>
2025-11-23 14:42:25 +0000

    _fetchRecords(): Update re phraseLower slot

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,21 @@
-import type { PhraseBookRecord } from "./model";
+import type { PhraseBookRecord, PhraseNub } from "./model";
+
+const _addLower = (phrase: PhraseNub) => {
+  phrase.phraseLower = phrase.phrase.toLowerCase();
+};
 
 const _fetchRecords = async () => {
   const resp = await fetch("phrase-book.json");
-  const untypedData = (await resp.json()) as Array<unknown>;
-  return untypedData.map((obj, id) =>
-    Object.assign({}, obj, { id })
-  ) as Array<PhraseBookRecord>;
+  const untypedData = (await resp.json()) as Array<any>;
+
+  untypedData.forEach((obj, id) => {
+    obj.id = id;
+    _addLower(obj.keyPhrase);
+    obj.altKeyPhrases.forEach(_addLower);
+    obj.valuePhrases.forEach(_addLower);
+  });
+
+  return untypedData as Array<PhraseBookRecord>;
 };
 
 let _records: Promise<Array<PhraseBookRecord>> | null = null;
