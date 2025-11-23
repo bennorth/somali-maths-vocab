dbde016603a652f7fc394bf7eb91e41cbaa85479
Ben North <ben@redfrontdoor.org>
2025-11-23 11:39:21 +0000

    About: Define/include component

---
+++ src/About.tsx
@@ -0,0 +1,10 @@
+export const About: React.FC<unknown> = () => {
+  return (
+    <div className="_About">
+      <span>Somali/English maths vocab</span>
+      <span>
+        <a href="https://github.com/bennorth/somali-maths-vocab">About</a>
+      </span>
+    </div>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { About } from "./About";
 import "./App.scss";
 import { VocabTable } from "./VocabTable";
 
 function App() {
   return (
     <div className="_App">
+      <About />
       <VocabTable />
     </div>
   );
